8821f11b12feb32fc7b02313eca2c255815f8d3b
mochimochistore <mochimochistore@gmail.com>
2019-02-11 23:52:01 +0900

    パッケージ名変更。

--- app/src/androidTest/java/jp/ac/yuge/proximity/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/jp/ac/yuge/proximity/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package jp.ac.kagawanct.proximity;
+package jp.ac.yuge.proximity;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("jp.ac.kagawanct.proximity", appContext.getPackageName());
+        assertEquals("jp.ac.yuge.proximity", appContext.getPackageName());
     }
 }
